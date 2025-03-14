30dc6f79c55b5b8efc8b10d760ddec6e0c4cd5eb
Akshat <82810411+Afterdie@users.noreply.github.com>
2025-03-14 20:45:23 +0530

    with firebase

---
+++ src/app/firebaseConfig.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClnydrsNQ6EBqvlu3BppQdAJEg2mB-y_o",
+  authDomain: "squeal-6e970.firebaseapp.com",
+  projectId: "squeal-6e970",
+  storageBucket: "squeal-6e970.firebasestorage.app",
+  messagingSenderId: "487938952754",
+  appId: "1:487938952754:web:ee76739843829c44ebf62b",
+  measurementId: "G-XQ1GZ7PY1M",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
